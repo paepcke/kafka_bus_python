bf574e762a95f18dd6ce07b22c17f0aba9f1ffae
Andreas Paepcke <paepcke@cs.stanford.edu>
2015-05-22 19:17:24 -0700

    Finished unittests for BusAdapter. Added example_consumer.py and example_producer.py. These two get FailedPayloadsError periodically. Must either catch those, or find the config setting that prevents them. Supposedly it's a timeout setting for sockets.

--- src/kafka_bus_python/kafka_bus.py
+++ src/kafka_bus_python/kafka_bus.py
@@ -17,27 +17,27 @@ from kafka_bus_python.topic_waiter import TopicWaiter
 
 class BusAdapter(object):
     '''
-    classdocs
+    The BusAdapter class is intended to be imported to bus modules.
+    
     '''
     DEFAULT_KAFKA_LISTEN_PORT = 9092
 
-    def __init__(self, host='localhost', port=None, moduleName=None):
+    def __init__(self, kafkaHost='localhost', kafkaPort=None):
         '''
-        
-        :param host:
-        :type host:
-        :param port:
-        :type port:
-        :param moduleName:
-        :type moduleName:
-        '''
-        self.host = host
-        if port is None:
-            port = BusAdapter.DEFAULT_KAFKA_LISTEN_PORT
-        self.port = port
-        self.moduleName = moduleName
+        Initialize communications with Kafka.
 
-        self.kafkaClient = KafkaClient("%s:%s" % (host,port))
+        :param kafkaHost: hostname or ip address of host where Kafka and Zookeeper hosts run
+        :type kafkaHost: string
+        :param kafkaPort: port at which Kafka expects clients to come in
+        :type kafkaPort: int
+        '''
+
+        self.host = kafkaHost
+        if kafkaPort is None:
+            kafkaPort = BusAdapter.DEFAULT_KAFKA_LISTEN_PORT
+        self.port = kafkaPort
+
+        self.kafkaClient = KafkaClient("%s:%s" % (kafkaHost,kafkaPort))
         self.producer    = SimpleProducer(self.kafkaClient)
 
         # Create a function that has the first method-arg
@@ -323,30 +323,3 @@ class BusAdapter(object):
         self.topicEvents.clear()
         
         self.kafkaClient.close()
-
-    
-    
-# from kafka import SimpleProducer, KafkaClient
-# from random import randint
-# 
-# # Producer module sends out Kafka messages on port 9092
-# k = KafkaClient("localhost:9092")
-# producer = SimpleProducer(k)
-# 
-# # User specifies name for event log (e.g. name of module or activity)
-# title = str(raw_input("Name event log: "))
-# k.ensure_topic_exists(title)
-# 
-# # Unique user sends messages as needed.
-# uid = "Learner" + str(randint(0, 20000))
-# while True:
-#     event = raw_input("Add what to event log?: ('Q' to end.): ")
-#     if event == 'Q':
-#         break
-#     else:
-#         msg = event.encode('UTF-8', 'ignore')
-#         producer.send_messages(title, "%s: %s" % (uid, msg))
-# 
-# # Module closes connection on exit.
-# k.close()
-    
